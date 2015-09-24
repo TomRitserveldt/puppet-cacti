8adfe6504ca1857a2974baef1400f96ab109fe53
Tom Ritserveldt <muffin@inuits.be>
2015-09-24 13:30:54 +0200

    wip - place remove_device script on cacti node

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,14 @@
-class cacti {
+class cacti(
+  $cli_dir = '/usr/share/cacti/cli',
+) {
 
   package { "cacti" :
         ensure => installed,
   }
 
-
+  file { '${cli_dir}/remove_device.php':
+    ensure => present,
+    source => 'puppet:///modules/cacti/remove_device.php',
+  }
 
 }

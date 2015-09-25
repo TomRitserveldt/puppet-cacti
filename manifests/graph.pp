8bbde8a472a6ee3c6330b4e728b8ca57b8732ba5
Tom Ritserveldt <muffin@inuits.be>
2015-09-25 09:48:21 +0200

    graphs.pp cleanup

--- manifests/graph.pp
+++ manifests/graph.pp
@@ -1,28 +1,17 @@
-tion: cacti::graph
+# == Definition: cacti::graph
 #
 define cacti::graph (
-  $ip           = undef,
-  $ensure       = 'present',
-  $disable      = 0,
-  $description  = undef,
-  $template     = 8,
-  $notes        = '',
-  $disable      = 0,
-  $avail        = 'snmp',
-  $ping_method  = 'tcp',
-  $ping_port    = 161,
-  $ping_retries = 2,
-  $version      = 2,
-  $port         = 161,
-  $timeout      = 500,
-  $community    = 'mgmtcacti',
-  $username     = '',
-  $password     = '',
-  $authproto    = '',
-  $privpass     = '',
-  $privproto    = ''
-) {
+  $cli_dir = $cacti::params::cli_dir,
+  $host = $cacti::params::host,
+  $graphtype = $cacti::params::graphtype,
+  $graphtemplate = $cacti::params::graphtemplate,
+  $graphtitle=$name
+) inherits cacti::params {
 
+  exec { "add_graph_${host}_${graphtemplate}_${field}":
+    command => "${cli_dir}add_graphs '${host}' ${graphtype} '${graphtemplate}' '${graphtitle}' '${field}' '${snmpquery}' '${snmpqtype}' '${snmpvalue}' '${reindexmethod}'",
+    unless  => "php -q ${cli_dir}add_tree.php --list-hosts | grep '${host}' | awk '{print \$1}' | xargs -I++ php -q /data/web/cacti/htdocs/cli/add_tree.php --list-graphs --host-id=++ | grep '${graphtitle}'",
+  }
 
 
 }

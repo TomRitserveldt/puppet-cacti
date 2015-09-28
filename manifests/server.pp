737c95f9fa7dbece733dbfe7413d71b2380fac3e
Tom Ritserveldt <muffin@inuits.be>
2015-09-28 14:44:34 +0200

    ensures httpd is running for cacti server

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,7 +2,11 @@ class cacti::server {
 
   package { "cacti" :
           ensure => $ensure,
-    }
+  } ->
+  service { 'httpd':
+    ensure     => running,
+    enable     => true,
+  }
   Cacti::Host <<| title!= undef |>>
   Cacti::Device <<| title != undef |>>
   Cacti::Graph <<| title != undef |>>

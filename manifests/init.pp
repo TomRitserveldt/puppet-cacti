37da5da081aad87b5c4dd6b83046822c713cada8
Tom Ritserveldt <muffin@inuits.be>
2015-09-28 09:01:23 +0200

    wip

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,9 @@ class cacti(
     source => 'puppet:///modules/cacti/remove_device.php',
   }
 
+  file { "/usr/share/cacti/scripts":
+    ensure => directory,
+  } ->
   file { "/usr/share/cacti/scripts/cactigraph.sh":
     ensure => present,
     source => 'puppet:///modules/cacti/cactigraph.sh',
